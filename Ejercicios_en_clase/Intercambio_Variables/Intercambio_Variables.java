bde074f55cd1c6a55c3223ab17c553b9c3a1f505
Leyber <159706335+LeyberP@users.noreply.github.com>
2026-08-20 12:21:14 -0500

    Add files via upload

---
+++ Ejercicios_en_clase/Intercambio_Variables/Intercambio_Variables.java
@@ -0,0 +1,11 @@
+public class Intercambio_Variables {
+    public static void main(String[] args) {
+    int Vrbl1=5, Vrbl2=10, temp;
+    temp=Vrbl1;
+    Vrbl1=Vrbl2;
+    Vrbl2=temp;
+        System.out.println("Vrbl1= "+Vrbl1);
+        System.out.println("Vrbl2= "+Vrbl2);
+    }
+    
+    }
